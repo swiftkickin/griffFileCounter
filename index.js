7bdb026a91f26f9153960473fe1d4643cd72f017
Kevin Griffin <kevin@kevgriffin.com>
2018-12-14 12:09:09 -0500

    yay package'

---
+++ index.js
@@ -0,0 +1,11 @@
+var fileCounter = (function (){
+    return {
+        count: () => {
+            console.log("How the heck should I know?");
+        }
+    }
+})();
+
+export default fileCounter;
+
+
